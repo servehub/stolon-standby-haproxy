41c5af9afeb99e5068e935b97cd4803368af18f7
kulikov <kulikov.dm@gmail.com>
2019-12-16 09:53:08 +0300

    init

---
+++ Dockerfile
@@ -0,0 +1,42 @@
+FROM haproxy:2.1.1
+
+LABEL maintainer="Anton Markelov <a.markelov@unitedtraders.com>"
+
+ENV PYTHONUNBUFFERED 1
+ENV DEBIAN_FRONTEND noninteractive
+
+RUN apt-get update && \
+  apt-get install -y --no-install-recommends \
+  python3 \
+  python3-dev \
+  python3-pip \
+  python3-setuptools \
+  supervisor \
+  curl && \
+  apt-get -y clean && \
+  rm -rf /var/lib/apt/lists/*
+
+COPY requirements.txt /app/requirements.txt
+RUN pip3 install --no-cache-dir -r /app/requirements.txt
+
+ENV STOLON_VERSION="0.14.0"
+
+RUN curl -Lfs https://github.com/sorintlab/stolon/releases/download/v${STOLON_VERSION}/stolon-v${STOLON_VERSION}-linux-amd64.tar.gz \
+      -o /tmp/stolon.tar.gz \
+    && tar -xzf /tmp/stolon.tar.gz -C /tmp \
+    && cp /tmp/stolon-v${STOLON_VERSION}-linux-amd64/bin/* /usr/bin/ \
+    && rm -rf /tmp/stolon*
+
+COPY docker/config.yml /app/config.yml
+
+COPY docker/haproxy.cfg /usr/local/etc/haproxy/config.cfg
+COPY docker/stolon_haproxy.j2 /app/stolon_haproxy.j2
+RUN touch /usr/local/etc/haproxy/stolon-config.cfg
+
+COPY docker/supervisord.conf /app/supervisord.conf
+
+COPY src /app/src
+
+EXPOSE 25433
+
+CMD ["supervisord", "-c", "/app/supervisord.conf"]
---
+++ src/stolon_haproxy.py
@@ -0,0 +1,97 @@
+import json
+import os
+import sys
+import time
+import yaml
+from jinja2 import Template
+from subprocess import check_output, run
+
+
+def read_config(config_file):
+    input_file = open(config_file, 'rb')
+    return yaml.load(input_file.read())
+
+
+def check_env_variables():
+    need_env = ['STOLONCTL_CLUSTER_NAME', 'STOLONCTL_STORE_BACKEND', 'STOLONCTL_STORE_ENDPOINTS']
+    for ne in need_env:
+        if ne not in os.environ:
+            sys.stderr.write("Please set {} environment variable".format(ne))
+            sys.exit(1)
+
+
+# get_stolon_servers accepts a JSON from stolonctl utility and returns list of servers available to connect
+def get_stolon_servers(stolon_json, fallback_to_master=False):
+    server_list = []
+
+    # Adding support for newer version stolon clusterdata format
+    if 'DBs' in stolon_json:
+        key = 'DBs'
+    else:
+        key = 'dbs'
+
+    # get standby's
+    for db in stolon_json[key]:
+        database = stolon_json[key][db]
+        if 'healthy' in database['status'] and 'listenAddress' in database['status']:
+            if database['status']['healthy']:
+                if database['spec']['role'] == 'standby':
+                    server_list.append(
+                        database['status']['listenAddress'] + ':' + database['status']['port'])
+                else:
+                    master_address = database['status']['listenAddress'] + ':' + database['status']['port']
+
+    if server_list == [] and fallback_to_master:
+        server_list.append(master_address)
+
+    return server_list
+
+
+if __name__ == '__main__':
+    print("Starting...")
+
+    if len(sys.argv) != 2:
+        print("Usage: %s <yaml config>" % sys.argv[0])
+        sys.exit(-1)
+
+    # read config
+    config = read_config(sys.argv[1])
+    check_env_variables()
+
+    haproxy_template = open('./stolon_haproxy.j2', 'r')
+    template = Template(haproxy_template.read())
+    haproxy_template.close()
+
+    while True:
+
+        output = check_output("stolonctl clusterdata read", shell=True)
+
+        try:
+            stolon_json = json.loads(output)
+        except Exception as e:
+            print("Error output from stolonctl: %s" % output)
+            time.sleep(config['timeout'])
+            continue
+
+        standby_list = get_stolon_servers(stolon_json, config['fallback_to_master'])
+
+        # if np servers to route - skip this iteration and print the error
+        if not standby_list:
+            print("No available backends!")
+            continue
+
+        new_render = template.render(servers=standby_list, frontend_port=config['postgres_haproxy_port'])
+
+        haproxy_config = open(config['postgres_haproxy_config'], 'r')
+
+        if haproxy_config.read() != new_render:
+            print("Config changed!")
+            haproxy_config.close()
+            haproxy_config = open(config['postgres_haproxy_config'], 'w')
+            haproxy_config.write(new_render)
+            run(config['haproxy_reload_command'], shell=True, check=True)
+
+        haproxy_config.close()
+        haproxy_template.close()
+
+        time.sleep(config['timeout'])
